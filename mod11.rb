951b90a6a1a4054fd98ec129736fcb885b71e066
Marshall Mickelson <marshallmick+git@gmail.com>
2009-08-25 22:17:42 -0500

    Refactored out weight into a yield emitter and an array colletor

--- mod11.rb
+++ mod11.rb
@@ -6,7 +6,7 @@
 
 class Mod11
     
-  def weight(number_of_digits)
+  def weight_emitter(number_of_digits)
     nextseq = 2
     number_of_digits.times do |x|
       yield nextseq
@@ -16,9 +16,23 @@ class Mod11
        end
     end
   end
+  
+  def weight(number_of_digits)
+    new_array = []
+    weight_emitter(number_of_digits) { |x| new_array << x }
+    new_array
+  end
+  
 end  
   
 m = Mod11.new
 
-m.weight(10){|x| puts x}
+#m.weight(10) { |x| puts x }
+
+w = m.weight(10)
+
+w.each do |x| puts x end
+
+#m.weight(10) { |x| puts x }
+
 
